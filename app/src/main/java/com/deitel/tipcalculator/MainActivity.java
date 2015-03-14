d942e79ffdfdc4474a4e617303508844a47f5f0e
12301113-ZhaoJiajin <12301113@bjtu.edu.cn>
2015-03-14 12:24:33 +0800

    tip-calculator 12301113-Zhao Jiajin

--- app/src/main/java/com/deitel/tipcalculator/MainActivity.java
+++ app/src/main/java/com/deitel/tipcalculator/MainActivity.java
@@ -3,171 +3,274 @@
 package com.deitel.tipcalculator;
 
 import java.text.NumberFormat; // for currency formatting
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.app.Activity; // base class for activities
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle; // for saving state information
 import android.text.Editable; // for EditText event handling
 import android.text.TextWatcher; // EditText listener
+import android.view.View;
 import android.widget.EditText; // for bill amount input
 import android.widget.SeekBar; // for changing custom tip percentage
 import android.widget.SeekBar.OnSeekBarChangeListener; // SeekBar listener
 import android.widget.TextView; // for displaying text
 
 // MainActivity class for the Tip Calculator app
-public class MainActivity extends Activity 
+public class MainActivity extends Activity
 {
-   // currency and percent formatters 
-   private static final NumberFormat currencyFormat = 
-      NumberFormat.getCurrencyInstance();
-   private static final NumberFormat percentFormat = 
-      NumberFormat.getPercentInstance();
+    // currency and percent formatters
+    private static final NumberFormat currencyFormat =
+            NumberFormat.getCurrencyInstance();
+    private static final NumberFormat percentFormat =
+            NumberFormat.getPercentInstance();
 
-   private double billAmount = 0.0; // bill amount entered by the user
-   private double customPercent = 0.18; // initial custom tip percentage
-   private TextView amountDisplayTextView; // shows formatted bill amount
-   private TextView percentCustomTextView; // shows custom tip percentage
-   private TextView tip15TextView; // shows 15% tip
-   private TextView total15TextView; // shows total with 15% tip
-   private TextView tipCustomTextView; // shows custom tip amount
-   private TextView totalCustomTextView; // shows total with custom tip
 
-   // called when the activity is first created
-   @Override
-   protected void onCreate(Bundle savedInstanceState)
-   {
-      super.onCreate(savedInstanceState); // call superclass's version
-      setContentView(R.layout.activity_main); // inflate the GUI
+    private double billAmount = 0.0; // bill amount entered by the user
+    private double customPercent = 0.18; // initial custom tip percentage
+    private TextView amountDisplayTextView; // shows formatted bill amount
+    private TextView percentCustomTextView; // shows custom tip percentage
+    private TextView tip15TextView; // shows 15% tip
+    private TextView total15TextView; // shows total with 15% tip
+    private TextView tipCustomTextView; // shows custom tip amount
+    private TextView totalCustomTextView; // shows total with custom tip
+    private EditText persons;
+    private int sumPerson = 1;
 
-      // get references to the TextViews 
-      // that MainActivity interacts with programmatically
-      amountDisplayTextView = 
-         (TextView) findViewById(R.id.amountDisplayTextView);
-      percentCustomTextView = 
-         (TextView) findViewById(R.id.percentCustomTextView);
-      tip15TextView = (TextView) findViewById(R.id.tip15TextView);
-      total15TextView = (TextView) findViewById(R.id.total15TextView);
-      tipCustomTextView = (TextView) findViewById(R.id.tipCustomTextView);
-      totalCustomTextView = 
-         (TextView) findViewById(R.id.totalCustomTextView);
-            
-      // update GUI based on billAmount and customPercent 
-      amountDisplayTextView.setText(
-         currencyFormat.format(billAmount));
-      updateStandard(); // update the 15% tip TextViews
-      updateCustom(); // update the custom tip TextViews
+    // called when the activity is first created
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState); // call superclass's version
+        setContentView(R.layout.activity_main); // inflate the GUI
 
-      // set amountEditText's TextWatcher
-      EditText amountEditText = 
-         (EditText) findViewById(R.id.amountEditText);
-      amountEditText.addTextChangedListener(amountEditTextWatcher);
-      
-      // set customTipSeekBar's OnSeekBarChangeListener
-      SeekBar customTipSeekBar = 
-         (SeekBar) findViewById(R.id.customTipSeekBar);
-      customTipSeekBar.setOnSeekBarChangeListener(customSeekBarListener);
-   } // end method onCreate
-   
-   // updates 15% tip TextViews
-   private void updateStandard() 
-   {
-      // calculate 15% tip and total
-      double fifteenPercentTip = billAmount * 0.15;
-      double fifteenPercentTotal = billAmount + fifteenPercentTip;
+        // get references to the TextViews
+        // that MainActivity interacts with programmatically
+        amountDisplayTextView =
+                (TextView) findViewById(R.id.amountDisplayTextView);
+        percentCustomTextView =
+                (TextView) findViewById(R.id.percentCustomTextView);
+        tip15TextView = (TextView) findViewById(R.id.tip15TextView);
+        total15TextView = (TextView) findViewById(R.id.total15TextView);
+        tipCustomTextView = (TextView) findViewById(R.id.tipCustomTextView);
+        persons = (EditText)findViewById(R.id.persons);
+        totalCustomTextView =
+                (TextView) findViewById(R.id.totalCustomTextView);
 
-      // display 15% tip and total formatted as currency
-      tip15TextView.setText(currencyFormat.format(fifteenPercentTip));
-      total15TextView.setText(currencyFormat.format(fifteenPercentTotal));
-   } // end method updateStandard
+        // update GUI based on billAmount and customPercent
+        amountDisplayTextView.setText(
+                currencyFormat.format(billAmount));
+        updateStandard(); // update the 15% tip TextViews
+        updateCustom(); // update the custom tip TextViews
 
-   // updates the custom tip and total TextViews
-   private void updateCustom() 
-   {
-      // show customPercent in percentCustomTextView formatted as %
-      percentCustomTextView.setText(percentFormat.format(customPercent));
+        // set amountEditText's TextWatcher
+        EditText amountEditText =
+                (EditText) findViewById(R.id.amountEditText);
+        amountEditText.addTextChangedListener(amountEditTextWatcher);
 
-      // calculate the custom tip and total
-      double customTip = billAmount * customPercent;
-      double customTotal = billAmount + customTip;
+        // set customTipSeekBar's OnSeekBarChangeListener
+        SeekBar customTipSeekBar =
+                (SeekBar) findViewById(R.id.customTipSeekBar);
+        customTipSeekBar.setOnSeekBarChangeListener(customSeekBarListener);
 
-      // display custom tip and total formatted as currency
-      tipCustomTextView.setText(currencyFormat.format(customTip));
-      totalCustomTextView.setText(currencyFormat.format(customTotal));
-   } // end method updateCustom
-   
-   // called when the user changes the position of SeekBar
-   private OnSeekBarChangeListener customSeekBarListener = 
-      new OnSeekBarChangeListener() 
-   {
-      // update customPercent, then call updateCustom
-      @Override
-      public void onProgressChanged(SeekBar seekBar, int progress,
-         boolean fromUser) 
-      {
-         // sets customPercent to position of the SeekBar's thumb
-         customPercent = progress / 100.0;
-         updateCustom(); // update the custom tip TextViews
-      } // end method onProgressChanged
+        persons.addTextChangedListener(personSumWatcher);
+        //persons.setOnFocusChangeListener(personSumWatcher);
+    } // end method onCreate
 
-      @Override
-      public void onStartTrackingTouch(SeekBar seekBar) 
-      {
-      } // end method onStartTrackingTouch
+    // updates 15% tip TextViews
+    private void updateStandard()
+    {
+        // calculate 15% tip and total
+        //sumPerson = Integer.parseInt(persons.getText().toString());
 
-      @Override
-      public void onStopTrackingTouch(SeekBar seekBar) 
-      {
-      } // end method onStopTrackingTouch
-   }; // end OnSeekBarChangeListener
+        double fifteenPercentTip = billAmount * 0.15;
+        double fifteenPercentTotal = (billAmount + fifteenPercentTip)/sumPerson;
 
-   // event-handling object that responds to amountEditText's events
-   private TextWatcher amountEditTextWatcher = new TextWatcher() 
-   {
-      // called when the user enters a number
-      @Override
-      public void onTextChanged(CharSequence s, int start, 
-         int before, int count) 
-      {         
-         // convert amountEditText's text to a double
-         try
-         {
-            billAmount = Double.parseDouble(s.toString()) / 100.0;
-         } // end try
-         catch (NumberFormatException e)
-         {
-            billAmount = 0.0; // default if an exception occurs
-         } // end catch 
+        // display 15% tip and total formatted as currency
+        tip15TextView.setText(currencyFormat.format(fifteenPercentTip));
+        total15TextView.setText(currencyFormat.format(fifteenPercentTotal));
+    } // end method updateStandard
 
-         // display currency formatted bill amount
-         amountDisplayTextView.setText(currencyFormat.format(billAmount));
-         updateStandard(); // update the 15% tip TextViews
-         updateCustom(); // update the custom tip TextViews
-      } // end method onTextChanged
+    // updates the custom tip and total TextViews
+    private void updateCustom()
+    {
+        //if (persons.getText().toString() != "")
+        //sumPerson = Integer.parseInt(persons.getText().toString());
+        // show customPercent in percentCustomTextView formatted as %
+        percentCustomTextView.setText(percentFormat.format(customPercent));
 
-      @Override
-      public void afterTextChanged(Editable s) 
-      {
-      } // end method afterTextChanged
+        // calculate the custom tip and total
+        double customTip = billAmount * customPercent;
+        double customTotal = (billAmount + customTip)/sumPerson;
 
-      @Override
-      public void beforeTextChanged(CharSequence s, int start, int count,
-         int after) 
-      {
-      } // end method beforeTextChanged
-   }; // end amountEditTextWatcher
+        // display custom tip and total formatted as currency
+        tipCustomTextView.setText(currencyFormat.format(customTip));
+        totalCustomTextView.setText(currencyFormat.format(customTotal));
+    } // end method updateCustom
+
+    // called when the user changes the position of SeekBar
+    private OnSeekBarChangeListener customSeekBarListener =
+            new OnSeekBarChangeListener()
+            {
+                // update customPercent, then call updateCustom
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,
+                                              boolean fromUser)
+                {
+                    // sets customPercent to position of the SeekBar's thumb
+                    customPercent = progress / 100.0;
+                    updateCustom(); // update the custom tip TextViews
+                } // end method onProgressChanged
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar)
+                {
+                } // end method onStartTrackingTouch
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar)
+                {
+                } // end method onStopTrackingTouch
+            }; // end OnSeekBarChangeListener
+
+    // event-handling object that responds to amountEditText's events
+    private TextWatcher amountEditTextWatcher = new TextWatcher()
+    {
+        // called when the user enters a number
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            // convert amountEditText's text to a double
+
+            try
+            {
+                billAmount = Double.parseDouble(s.toString()) / 100.0;
+            } // end try
+            catch (NumberFormatException e)
+            {
+                billAmount = 0.0; // default if an exception occurs
+            } // end catch
+
+            // display currency formatted bill amount
+            amountDisplayTextView.setText(currencyFormat.format(billAmount));
+            updateStandard(); // update the 15% tip TextViews
+            updateCustom(); // update the custom tip TextViews
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    }; // end amountEditTextWatcher
+
+    private TextWatcher personSumWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            //sumPerson = Integer.parseInt(persons.getText().toString());
+            String partySizeStr = "";
+            try
+            {
+                if((persons != null) && !("".equals(persons))){
+                    partySizeStr = persons.getText().toString();
+                }
+
+            } // end try
+            catch (Exception e)
+            {
+                sumPerson = 1; // default if an exception occurs
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("warning");
+                builder.setPositiveButton("ok",null);
+                builder.setIcon(android.R.drawable.ic_dialog_info);
+                builder.setMessage("please enter a integer larger than 1");
+                builder.show();
+            } // end catch
+            if(isInteger(partySizeStr)){
+                sumPerson = Integer.parseInt(partySizeStr);
+                if(sumPerson >= 1){
+                    updateStandard(); // update the 15% tip TextViews
+                    updateCustom(); // update the custom tip TextViews
+                }
+                else{
+                    sumPerson = 1; // default if an exception occurs
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("warning");
+                    builder.setPositiveButton("ok",null);
+                    builder.setIcon(android.R.drawable.ic_dialog_info);
+                    builder.setMessage("please enter a integer larger than 1");
+                    builder.show();
+                }
+            }
+            else{
+                sumPerson = 1; // default if an exception occurs
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("warning");
+                builder.setPositiveButton("ok",null);
+                builder.setIcon(android.R.drawable.ic_dialog_info);
+                builder.setMessage("please enter a integer larger than 1");
+                builder.show();
+            }
+
+        }
+    };
+    /*private View.OnFocusChangeListener personSumWatcher = new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            sumPerson = Integer.parseInt(persons.getText().toString());
+            try
+            {
+                if((persons != null) && !("".equals(persons))){
+                    sumPerson = Integer.parseInt(persons.getText().toString());
+                }
+
+            } // end try
+            catch (Exception e)
+            {
+                sumPerson = 1; // default if an exception occurs
+            } // end catch
+            updateStandard(); // update the 15% tip TextViews
+            updateCustom(); // update the custom tip TextViews
+        }
+    };*/
+
+    private static boolean isInteger(String input){
+        Matcher mer = Pattern.compile("^[+-]?[0-9]+$").matcher(input);
+        return mer.find();
+    }
 } // end class MainActivity
 
 
 /*************************************************************************
-* (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
-* Pearson Education, Inc. All Rights Reserved.                           *
-*                                                                        *
-* DISCLAIMER: The authors and publisher of this book have used their     *
-* best efforts in preparing the book. These efforts include the          *
-* development, research, and testing of the theories and programs        *
-* to determine their effectiveness. The authors and publisher make       *
-* no warranty of any kind, expressed or implied, with regard to these    *
-* programs or to the documentation contained in these books. The authors *
-* and publisher shall not be liable in any event for incidental or       *
-* consequential damages in connection with, or arising out of, the       *
-* furnishing, performance, or use of these programs.                     *
-*************************************************************************/
+ * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ *************************************************************************/
