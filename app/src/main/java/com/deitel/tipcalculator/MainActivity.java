173fc5e59c6202596c089334a15e5438b31ed912
12301001-CaoDijia <12301001@bjtu.edu.cn>
2015-03-16 11:12:29 +0800

    tip-calculator

--- app/src/main/java/com/deitel/tipcalculator/MainActivity.java
+++ app/src/main/java/com/deitel/tipcalculator/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends Activity
 
    private double billAmount = 0.0; // bill amount entered by the user
    private double customPercent = 0.18; // initial custom tip percentage
+   private int peopleAmount = 1; // people amount entered by the user
+   private TextView numberDisplayTextView; // shows formatted people amount
    private TextView amountDisplayTextView; // shows formatted bill amount
    private TextView percentCustomTextView; // shows custom tip percentage
    private TextView tip15TextView; // shows 15% tip
@@ -154,6 +156,41 @@ public class MainActivity extends Activity
       {
       } // end method beforeTextChanged
    }; // end amountEditTextWatcher
+    private TextWatcher peopleEditTextWatcher = new TextWatcher()
+    {
+        // called when the user enters a number
+        @Override
+        public void onTextChanged(CharSequence s, int start,
+                                  int before, int count)
+        {
+            // convert amountEditText's text to a double
+            try
+            {
+                peopleAmount = Integer.parseInt(s.toString());
+            } // end try
+            catch (NumberFormatException e)
+            {
+                peopleAmount = 1; // default if an exception occurs
+            } // end catch
+
+            // display currency formatted bill amount
+//      amountDisplayTextView.setText(currencyFormat.format(billAmount));
+            numberDisplayTextView.setText(peopleAmount+"");
+            updateStandard(); // update the 15% tip TextViews
+            updateCustom(); // update the custom tip TextViews
+        } // end method onTextChanged
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+        } // end method afterTextChanged
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                                      int after)
+        {
+        } // end method beforeTextChanged
+    }; // end peopleEditTextWatcher
 } // end class MainActivity
 
 
